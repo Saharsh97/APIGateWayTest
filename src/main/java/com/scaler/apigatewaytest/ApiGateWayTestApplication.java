c1b80338ebb63921112e4cb9edc485b8b2a54064
saharsh.s <saharsh.s@media.net>
2024-03-20 18:51:38 +0530

    init commit for API GateWay

---
+++ src/main/java/com/scaler/apigatewaytest/ApiGateWayTestApplication.java
@@ -0,0 +1,13 @@
+package com.scaler.apigatewaytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGateWayTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGateWayTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/scaler/apigatewaytest/ApiGateWayTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scaler.apigatewaytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGateWayTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
